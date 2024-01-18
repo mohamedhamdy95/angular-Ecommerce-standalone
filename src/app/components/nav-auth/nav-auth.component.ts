7e14b49bd55740888034088ba663fac96115e0fb
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-01-18 16:53:50 +0200

    three commit

--- src/app/components/nav-auth/nav-auth.component.ts
+++ src/app/components/nav-auth/nav-auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLinkActive,RouterLink],
   templateUrl: './nav-auth.component.html',
   styleUrls: ['./nav-auth.component.scss']
 })
--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavAuthComponent } from 'src/app/components/nav-auth/nav-auth.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from 'src/app/components/footer/footer.component';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [CommonModule,NavAuthComponent,RouterOutlet],
+  imports: [CommonModule,NavAuthComponent,FooterComponent,RouterOutlet],
   templateUrl: './auth-layout.component.html',
   styleUrls: ['./auth-layout.component.scss']
 })
