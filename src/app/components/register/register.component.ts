7a7b018f79b51975075b6b81a249fbc3243ae994
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-01-24 16:07:46 +0200

    24/1

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from 'src/app/core/service/auth.service';
-import { FormControl, FormControlOptions, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormControlOptions, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -11,7 +13,17 @@ import { FormControl, FormControlOptions, FormGroup, ReactiveFormsModule, Valida
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-  constructor(private _AuthService:AuthService){}
+  constructor(private _AuthService:AuthService , private _Router:Router , private _FormBuilder:FormBuilder){}
+  errMesg:string="";
+  sucMesg:string="";
+  isLoading:boolean = false;
+//**************** registerForm whith form builder ************
+  // registerForm : FormGroup = this._FormBuilder.group({
+  //   name : ['',[Validators.required, Validators.minLength(5),Validators.maxLength(30)]],
+  //   email : ['',[Validators.required,Validators.email] ,
+  // })
+
+
 
   registerForm : FormGroup = new FormGroup({
     name : new FormControl('',[Validators.required , Validators.minLength(5),Validators.maxLength(30)]),
@@ -34,8 +46,30 @@ export class RegisterComponent {
     }
   }
 
-  handleForm(){
-    console.log(this.registerForm.value)
+
+
+  handleForm():void{
+    const userData = this.registerForm.value;
+    this.isLoading=true;
+    if(this.registerForm.valid === true){
+      this._AuthService.register(userData).subscribe({
+        next:(respons)=>{
+          this.isLoading=false;
+          if(respons.message === "success"){
+            this._Router.navigate(['/login'])
+          }
+         this.sucMesg = respons.message
+          // console.log(respons)
+        },
+        error:(err)=>{
+          this.errMesg = err.error.message;
+          this.isLoading=false;
+          // console.log(this.errMesg)
+        },
+        complete:()=>{}
+      })
+    }
+    console.log(userData)
   }
 
 }
