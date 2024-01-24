f4356b1d336f7dfeb59fe4eb69bad1eb3c3ba42d
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-01-24 17:40:53 +0200

    24/1-2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormControl, Validators, FormControlOptions, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule ,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  constructor(private _AuthService:AuthService , private _Router:Router , private _FormBuilder:FormBuilder){}
+  errMesg:string="";
+  sucMesg:string="";
+  isLoading:boolean = false;
+  userToken:string='';
+//**************** loginForm whith form builder ************
+  // loginForm : FormGroup = this._FormBuilder.group({
+  //   name : ['',[Validators.required, Validators.minLength(5),Validators.maxLength(30)]],
+  //   email : ['',[Validators.required,Validators.email] ,
+  // })
+
+  loginForm : FormGroup = new FormGroup({
+    email : new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl('',[Validators.required,Validators.pattern(/^[a-zA-Z0-9_@]{6,}$/)]),
+  } )
+
+  handleForm():void{
+    const userData = this.loginForm.value;
+    this.isLoading=true;
+    if(this.loginForm.valid === true){
+      this._AuthService.login(userData).subscribe({
+        next:(respons)=>{
+          this.isLoading=false;
+          if(respons.message === "success"){
+            this.userToken=respons.token;
+            localStorage.setItem('usertoken',this.userToken)
+            this._AuthService.decodeUser();
+            this._Router.navigate(['/home'])
+          }
+         this.sucMesg = respons.message
+          // console.log(respons)
+        },
+        error:(err)=>{
+          this.errMesg = err.error.message;
+          this.isLoading=false;
+          // console.log(this.errMesg)
+        },
+        complete:()=>{}
+      })
+    }
+    console.log(userData)
+  }
 
 }
--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,15 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  userInfo:any;
   baseUrl:string='https://ecommerce.routemisr.com/api/v1/auth/';
   
   constructor(private _HttpClient:HttpClient) { }
   register(registerData:object):Observable<any>{
     return this._HttpClient.post(this.baseUrl+'signup',registerData)
   }
+  login(loginData:object):Observable<any>{
+    return this._HttpClient.post(this.baseUrl+'signin',loginData)
+  }
+
+  decodeUser(){
+    const encodeToken = localStorage.getItem('usertoken')
+    if(encodeToken !== null){
+      const decodeToken = jwtDecode(encodeToken)
+      this.userInfo = decodeToken;
+      // console.log(this.userInfo);
+    }
+  }
+
 }
