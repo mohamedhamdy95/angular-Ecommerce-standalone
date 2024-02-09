40c086ee69c198fe14bc0c5ab85474ed62423258
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-02-09 20:41:42 +0200

    9/2

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from 'src/app/core/service/cart.service';
+import { ProductService } from 'src/app/core/service/product.service';
+import { Product } from 'src/app/core/interface/product';
+import { RouterLink } from '@angular/router';
+import { CuttextPipe } from "../../core/pipe/cuttext.pipe";
 @Component({
-  selector: 'app-products',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+    selector: 'app-products',
+    standalone: true,
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss'],
+    imports: [CommonModule, RouterLink, CuttextPipe]
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  productList:Product[]=[]
+constructor(private _ProductService:ProductService , private _CartService:CartService , private toaster:ToastrService ,private _Renderer2:Renderer2){}
+ngOnInit(): void {
+  this._ProductService.getProduct().subscribe({
+    next:(respons)=>{
+      this.productList = respons.data;
+      // console.log(this.productList)
+    },
+    error:(err)=> {
+      console.log(err)
+    },
+  });
+}
+
+addProductToCart(id:any , ele:HTMLButtonElement ):void{
+  this._Renderer2.setAttribute(ele,'disabled','true');
+  this._CartService.addToCart(id).subscribe({
+    next:(respons)=>{
+      this.toaster.success(respons.message);
+      this._CartService.cartNumber.next(respons.numOfCartItems);
+      console.log(respons)
+    },
+    error:(err)=>{
+      console.log(err)
+    },
+    complete:()=>{
+      this._Renderer2.removeAttribute(ele,'disabled');
+    }
+  })
+}
+
 
 }
