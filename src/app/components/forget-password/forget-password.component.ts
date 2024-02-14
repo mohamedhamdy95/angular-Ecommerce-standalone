920f4c0758aead0438a0b691a5a58bd8368b2f4f
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-02-14 17:23:58 +0200

    14/2

--- src/app/components/forget-password/forget-password.component.ts
+++ src/app/components/forget-password/forget-password.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ForgetPassService } from 'src/app/core/service/forget-pass.service';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-forget-password',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './forget-password.component.html',
   styleUrls: ['./forget-password.component.scss']
 })
@@ -14,8 +15,10 @@ step1:boolean=true;
 step2:boolean=false;
 step3:boolean=false;
 
+constructor(private _ForgetPassService:ForgetPassService , private toaster:ToastrService){}
 
-forgetPass:FormGroup = new FormGroup({
+
+forgetPassForm:FormGroup = new FormGroup({
   email:new FormControl('',[Validators.required,Validators.email])
 })
 resetCode:FormGroup = new FormGroup({
@@ -24,6 +27,27 @@ resetCode:FormGroup = new FormGroup({
 resetPassword:FormGroup = new FormGroup({
   newPassword:new FormControl('',[Validators.required])
 })
-
-
+forgetPassword():void{
+  // this.step1 = false;
+  // this.step2=true;
+  let userEmail = this.forgetPassForm.value;
+  this._ForgetPassService.forgotPassword(userEmail).subscribe({
+    next:(respons)=>{
+      this.toaster.success(respons.message);
+      console.log(respons)
+    },
+    error:(err)=>{
+      console.log(err)
+    }
+  })
+  console.log('step1')
+}
+resetCodeFun():void{
+  this.step2=false;
+  this.step3=true;
+  console.log('step2')
+}
+resetpass():void{
+  console.log('step3')
+}
 }
---
+++ src/app/core/service/forget-pass.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForgetPassService {
+  baseUrl:string='https://ecommerce.routemisr.com/api/v1/auth/';
+  constructor(private _HttpClient: HttpClient) { }
+
+  
+  forgotPassword(userEmail:object):Observable<any>{
+    return this._HttpClient.post(this.baseUrl + `forgotPasswords` , userEmail)
+  }
+
+}
